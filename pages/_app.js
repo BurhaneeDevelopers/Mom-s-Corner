7e316d65261becd76746daf778c711502a9273d8
Mohammed <heavilgaming72@gmail.com>
2022-09-30 17:20:56 +0530

    ad

--- pages/_app.js
+++ pages/_app.js
@@ -21,9 +21,23 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Mom&apos;s Corner - The best guide for moms</title>
-        <meta name="description" content="We will guide you on how to take care of your baby. We offer information about the need for a new mother to take care of her baby and tips on what to do in each stage of your child's life." />
-        <meta name="keywords" content="baby, parenting tips, baby blog, blog,  baby health blogs, baby health articles, baby health website, best baby health blogs, best baby health websites, baby health sites" />
-        <meta name="google-site-verification" content="PD1qjR5OecZPYeiKxb0xIjC0DO3EJTIEEPIWCkpd4Mg" />
+        <meta
+          name="description"
+          content="We will guide you on how to take care of your baby. We offer information about the need for a new mother to take care of her baby and tips on what to do in each stage of your child's life."
+        />
+        <meta
+          name="keywords"
+          content="baby, parenting tips, baby blog, blog,  baby health blogs, baby health articles, baby health website, best baby health blogs, best baby health websites, baby health sites"
+        />
+        <meta
+          name="google-site-verification"
+          content="PD1qjR5OecZPYeiKxb0xIjC0DO3EJTIEEPIWCkpd4Mg"
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5575076125990523"
+          crossorigin="anonymous"
+        ></script>
         <link rel="icon" href="/favicon.jpg" />
       </Head>
       <LoadingBar
@@ -31,7 +45,7 @@ function MyApp({ Component, pageProps }) {
         progress={progress}
         height={5}
         className="rounded-full"
-        waitingTime = {400}
+        waitingTime={400}
         onLoaderFinished={() => setProgress(0)}
       />
       <Navbar />
