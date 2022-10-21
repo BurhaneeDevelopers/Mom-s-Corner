9598745b59bb38e7ee12e1634f2e2f382f3405c3
Mohammed <heavilgaming72@gmail.com>
2022-10-21 22:38:32 +0530

    errors-netlify

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home({ blogs }) {
       <label htmlFor="my-modal-1" className="modal cursor-pointer">
         <label
           className="modal-box relative bg-pink-200 w-11/12 max-w-5xl"
-          for=""
+         htmlFor=""
         >
           <label
             htmlFor="my-modal-1"
@@ -57,27 +57,31 @@ export default function Home({ blogs }) {
             Terms And Condition
           </h1>
           <div className="text-gray-900 text-justify mt-5">
-            <h3 className="text-lg font-bold mb-2">Welcome to Mom's Corner!</h3>
+            <h3 className="text-lg font-bold mb-2">
+              Welcome to Mom&apos;s Corner!
+            </h3>
 
             <p className="mb-2">
               These terms and conditions outline the rules and regulations for
-              the use of Mom's Corner's Website, located at
+              the use of Mom&apos;s Corner&apos;s Website, located at
               <a href="https://mommscorner.com">Mommscorner.com</a>
             </p>
 
             <p className="mb-2">
               By accessing this website we assume you accept these terms and
-              conditions. Do not continue to use Mom's Corner if you do not
+              conditions. Do not continue to use Mom&apos;s Corner if you do not
               agree to take all of the terms and conditions stated on this page.
             </p>
 
             <p className="mb-5">
               The following terminology applies to these Terms and Conditions,
               Privacy Statement and Disclaimer Notice and all Agreements:
-              "Client", "You" and "Your" refers to you, the person log on this
-              website and compliant to the Company’s terms and conditions. "The
-              Company", "Ourselves", "We", "Our" and "Us", refers to our
-              Company. "Party", "Parties", or "Us", refers to both the Client
+              &quot;Client&quot;, &quot;You&quot; and &quot;Your&quot; refers to
+              you, the person log on this website and compliant to the Company’s
+              terms and conditions. &quot;The Company&quot;,
+              &quot;Ourselves&quot;, &quot;We&quot;, &quot;Our&quot; and
+              &quot;Us&quot;, refers to our Company. &quot;Party&quot;,
+              &quot;Parties&quot;, or &quot;Us&quot;, refers to both the Client
               and ourselves. All terms refer to the offer, acceptance and
               consideration of payment necessary to undertake the process of our
               assistance to the Client in the most appropriate manner for the
@@ -94,47 +98,40 @@ export default function Home({ blogs }) {
             </h3>
 
             <p className="mb-2">
-              Unless otherwise stated, Mom's Corner and/or its licensors own the
-              intellectual property rights for all material on Mom's Corner. All
-              intellectual property rights are reserved. You may access this
-              from Mom's Corner for your own personal use subjected to
-              restrictions set in these terms and conditions.
+              Unless otherwise stated, Mom&apos;s Corner and/or its licensors
+              own the intellectual property rights for all material on
+              Mom&apos;s Corner. All intellectual property rights are reserved.
+              You may access this from Mom&apos;s Corner for your own personal
+              use subjected to restrictions set in these terms and conditions.
             </p>
 
             <p className="mb-2 font-bold">You must not:</p>
             <ul>
-              <li>Republish material from Mom's Corner</li>
-              <li>Sell, rent or sub-license material from Mom's Corner</li>
-              <li>Reproduce, duplicate or copy material from Mom's Corner</li>
-              <li>Redistribute content from Mom's Corner</li>
+              <li>Republish material from Mom&apos;s Corner</li>
+              <li>Sell, rent or sub-license material from Mom&apos;s Corner</li>
+              <li>
+                Reproduce, duplicate or copy material from Mom&apos;s Corner
+              </li>
+              <li>Redistribute content from Mom&apos;s Corner</li>
             </ul>
-
-            <p className="mb-2">
-              This Agreement shall begin on the date hereof. Our Terms and
-              Conditions were created with the help of the{" "}
-              <a href="https://h-supertools.com/web/terms-and-conditions-generator/">
-                H-supertools Terms And Conditions Generator
-              </a>
-              .
-            </p>
 
             <p className="mb-2">
               Parts of this website offer an opportunity for users to post and
               exchange opinions and information in certain areas of the website.
-              Mom's Corner does not filter, edit, publish or review Comments
-              prior to their presence on the website. Comments do not reflect
-              the views and opinions of Mom's Corner,its agents and/or
-              affiliates. Comments reflect the views and opinions of the person
-              who post their views and opinions. To the extent permitted by
-              applicable laws, Mom's Corner shall not be liable for the Comments
-              or for any liability, damages or expenses caused and/or suffered
-              as a result of any use of and/or posting of and/or appearance of
-              the Comments on this website.
+              Mom&apos;s Corner does not filter, edit, publish or review
+              Comments prior to their presence on the website. Comments do not
+              reflect the views and opinions of Mom&apos;s Corner,its agents
+              and/or affiliates. Comments reflect the views and opinions of the
+              person who post their views and opinions. To the extent permitted
+              by applicable laws, Mom&apos;s Corner shall not be liable for the
+              Comments or for any liability, damages or expenses caused and/or
+              suffered as a result of any use of and/or posting of and/or
+              appearance of the Comments on this website.
             </p>
 
             <p className="mb-2">
-              Mom's Corner reserves the right to monitor all Comments and to
-              remove any Comments which can be considered inappropriate,
+              Mom&apos;s Corner reserves the right to monitor all Comments and
+              to remove any Comments which can be considered inappropriate,
               offensive or causes breach of these Terms and Conditions.
             </p>
 
@@ -162,7 +159,7 @@ export default function Home({ blogs }) {
             </ul>
 
             <p className="mb-5">
-              You hereby grant Mom's Corner a non-exclusive license to use,
+              You hereby grant Mom&apos;s Corner a non-exclusive license to use,
               reproduce, edit and authorize others to use, reproduce and edit
               any of your Comments in any and all forms, formats or media.
             </p>
@@ -224,7 +221,7 @@ export default function Home({ blogs }) {
               ourselves or to our accredited businesses; (b) the organization
               does not have any negative records with us; (c) the benefit to us
               from the visibility of the hyperlink compensates the absence of
-              Mom's Corner; and (d) the link is in the context of general
+              Mom&apos;s Corner; and (d) the link is in the context of general
               resource information.
             </p>
 
@@ -239,11 +236,11 @@ export default function Home({ blogs }) {
             <p className="mb-2">
               If you are one of the organizations listed in paragraph 2 above
               and are interested in linking to our website, you must inform us
-              by sending an e-mail to Mom's Corner. Please include your name,
-              your organization name, contact information as well as the URL of
-              your site, a list of any URLs from which you intend to link to our
-              Website, and a list of the URLs on our site to which you would
-              like to link. Wait 2-3 weeks for a response.
+              by sending an e-mail to Mom&apos;s Corner. Please include your
+              name, your organization name, contact information as well as the
+              URL of your site, a list of any URLs from which you intend to link
+              to our Website, and a list of the URLs on our site to which you
+              would like to link. Wait 2-3 weeks for a response.
             </p>
 
             <p className="mb-2">
@@ -263,8 +260,8 @@ export default function Home({ blogs }) {
             </ul>
 
             <p className="mb-5">
-              No use of Mom's Corner's logo or other artwork will be allowed for
-              linking absent a trademark license agreement.
+              No use of Mom&apos;s Corner logo or other artwork will be
+              allowed for linking absent a trademark license agreement.
             </p>
 
             <h3 className="text-lg font-bold mb-2">
@@ -377,7 +374,7 @@ export default function Home({ blogs }) {
       <label htmlFor="my-modal-2" className="modal cursor-pointer">
         <label
           className="modal-box relative bg-pink-200 w-11/12 max-w-5xl"
-          for=""
+         htmlFor=""
         >
           <label
             htmlFor="my-modal-2"
@@ -388,11 +385,11 @@ export default function Home({ blogs }) {
           <h3 className="text-xl font-bold text-gray-900">Privacy Policy</h3>
           <div className="py-4 text-gray-900 text-justify">
             <h1 className="text-lg mb-2 font-bold">
-              Privacy Policy for Mom's Corner
+              Privacy Policy for Mom&apos;s Corner
             </h1>
 
             <p className="mb-5">
-              At Mom's Corner, accessible from{" "}
+              At Mom&apos;s Corner, accessible from{" "}
               <a
                 href="https://mommscorner.com"
                 className="text-indigo-700 hover:underline"
@@ -401,16 +398,16 @@ export default function Home({ blogs }) {
               </a>
               , one of our main priorities is the privacy of our visitors. This
               Privacy Policy document contains types of information that is
-              collected and recorded by Mom's Corner and how we use it. If you
-              have additional questions or require more information about our
-              Privacy Policy, do not hesitate to contact us.
+              collected and recorded by Mom&apos;s Corner and how we use it. If
+              you have additional questions or require more information about
+              our Privacy Policy, do not hesitate to contact us.
             </p>
 
             <p className="mb-5">
               This Privacy Policy applies only to our online activities and is
               valid for visitors to our website with regards to the information
-              that they shared and/or collect in Mom's Corner. This policy is
-              not applicable to any information collected offline or via
+              that they shared and/or collect in Mom&apos;s Corner. This policy
+              is not applicable to any information collected offline or via
               channels other than this website.
             </p>
 
@@ -466,15 +463,15 @@ export default function Home({ blogs }) {
             <h2 className="text-xl font-bold mb-2">Log Files</h2>
 
             <p className="mb-5">
-              Mom's Corner follows a standard procedure of using log files.
+              Mom&apos;s Corner follows a standard procedure of using log files.
               These files log visitors when they visit websites. All hosting
-              companies do this and a part of hosting services' analytics. The
+              companies do this and a part of hosting services analytics. The
               information collected by log files include internet protocol (IP)
               addresses, browser type, Internet Service Provider (ISP), date and
               time stamp, referring/exit pages, and possibly the number of
               clicks. These are not linked to any information that is personally
               identifiable. The purpose of the information is for analyzing
-              trends, administering the site, tracking users' movement on the
+              trends, administering the site, tracking users movement on the
               website, and gathering demographic information.
             </p>
 
@@ -521,18 +518,19 @@ export default function Home({ blogs }) {
 
             <p className="mb-2">
               You may consult this list to find the Privacy Policy for each of
-              the advertising partners of Mom's Corner. Third-party ad servers
-              or ad networks uses technologies like cookies, JavaScript, or Web
-              Beacons that are used in their respective advertisements and links
-              that appear on Mom's Corner, which are sent directly to users'
-              browser. They automatically receive your IP address when this
-              occurs. These technologies are used to measure the effectiveness
-              of their advertising campaigns and/or to personalize the
-              advertising content that you see on websites that you visit.
+              the advertising partners of Mom&apos;s Corner. Third-party ad
+              servers or ad networks uses technologies like cookies, JavaScript,
+              or Web Beacons that are used in their respective advertisements
+              and links that appear on Mom&apos;s Corner, which are sent
+              directly to users browser. They automatically receive your IP
+              address when this occurs. These technologies are used to measure
+              the effectiveness of their advertising campaigns and/or to
+              personalize the advertising content that you see on websites that
+              you visit.
             </p>
 
             <p className="mb-5">
-              Note that Mom's Corner has no access to or control over these
+              Note that Mom&apos;s Corner has no access to or control over these
               cookies that are used by third-party advertisers.
             </p>
 
@@ -541,17 +539,17 @@ export default function Home({ blogs }) {
             </h2>
 
             <p className="mb-2">
-              Mom's Corner's Privacy Policy does not apply to other advertisers
-              or websites. Thus, we are advising you to consult the respective
-              Privacy Policies of these third-party ad servers for more detailed
-              information. It may include their practices and instructions about
-              how to opt-out of certain options.{" "}
+              Mom&apos;s Corner Privacy Policy does not apply to other
+              advertisers or websites. Thus, we are advising you to consult the
+              respective Privacy Policies of these third-party ad servers for
+              more detailed information. It may include their practices and
+              instructions about how to opt-out of certain options.{" "}
             </p>
 
             <p className="mb-5">
               You can choose to disable cookies through your individual browser
               options. To know more detailed information about cookie management
-              with specific web browsers, it can be found at the browsers'
+              with specific web browsers, it can be found at the browsers
               respective websites.
             </p>
 
@@ -596,7 +594,7 @@ export default function Home({ blogs }) {
               would like to exercise any of these rights, please contact us.
             </li>
 
-            <h2 className="text-xl font-bold mb-2">Children's Information</h2>
+            <h2 className="text-xl font-bold mb-2">Children&apos;s Information</h2>
 
             <p className="mb-2">
               Another part of our priority is adding protection for children
@@ -606,12 +604,12 @@ export default function Home({ blogs }) {
             </p>
 
             <p>
-              Mom's Corner does not knowingly collect any Personal Identifiable
-              Information from children under the age of 13. If you think that
-              your child provided this kind of information on our website, we
-              strongly encourage you to contact us immediately and we will do
-              our best efforts to promptly remove such information from our
-              records.
+              Mom&apos;s Corner does not knowingly collect any Personal
+              Identifiable Information from children under the age of 13. If you
+              think that your child provided this kind of information on our
+              website, we strongly encourage you to contact us immediately and
+              we will do our best efforts to promptly remove such information
+              from our records.
             </p>
           </div>
         </label>
@@ -701,7 +699,7 @@ export default function Home({ blogs }) {
                   <span className="inline-block rounded-full">
                     <Image
                       src="/logo.png"
-                      alt="mom's corner"
+                      alt="Mom's corner"
                       width={150}
                       height={100}
                     />
