c0cbe91ba9dcd03dbf3e020d08af28244f38a12d
Mohammed <heavilgaming72@gmail.com>
2022-10-25 09:55:18 +0530

    terms&privacy

---
+++ components/privacy-policy.js
@@ -0,0 +1,262 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <>
+      {/* ---privay-policy--- */}
+      <input type="checkbox" id="my-modal-2" className="modal-toggle" />
+      <label htmlFor="my-modal-2" className="modal cursor-pointer">
+        <label
+          className="modal-box relative bg-pink-200 w-11/12 max-w-5xl"
+          htmlFor=""
+        >
+          <label
+            htmlFor="my-modal-2"
+            className="btn btn-sm btn-circle absolute right-2 top-2 bg-pink-400 text-gray-900 border-0 hover:bg-pink-600 hover:text-white"
+          >
+            <span className="mt-1"> ✕ </span>
+          </label>
+          <h3 className="text-xl font-bold text-gray-900">Privacy Policy</h3>
+          <div className="py-4 text-gray-900 text-justify">
+            <h1 className="text-lg mb-2 font-bold">
+              Privacy Policy for Mom&apos;s Corner
+            </h1>
+
+            <p className="mb-5">
+              At Mom&apos;s Corner, accessible from{" "}
+              <a
+                href="https://mommscorner.com"
+                className="text-indigo-700 hover:underline"
+              >
+                Mommscorner.com
+              </a>
+              , one of our main priorities is the privacy of our visitors. This
+              Privacy Policy document contains types of information that is
+              collected and recorded by Mom&apos;s Corner and how we use it. If
+              you have additional questions or require more information about
+              our Privacy Policy, do not hesitate to contact us.
+            </p>
+
+            <p className="mb-5">
+              This Privacy Policy applies only to our online activities and is
+              valid for visitors to our website with regards to the information
+              that they shared and/or collect in Mom&apos;s Corner. This policy
+              is not applicable to any information collected offline or via
+              channels other than this website.
+            </p>
+
+            <h2 className="text-xl font-bold mb-2">Consent</h2>
+
+            <p className="mb-5">
+              By using our website, you hereby consent to our Privacy Policy and
+              agree to its terms.
+            </p>
+
+            <h2 className="text-xl font-bold mb-2">Information we collect</h2>
+
+            <p className="mb-2">
+              The personal information that you are asked to provide, and the
+              reasons why you are asked to provide it, will be made clear to you
+              at the point we ask you to provide your personal information. If
+              you contact us directly, we may receive additional information
+              about you such as your name, email address, phone number, the
+              contents of the message and/or attachments you may send us, and
+              any other information you may choose to provide.
+            </p>
+            <p className="mb-5">
+              When you register for an Account, we may ask for your contact
+              information, including items such as name, company name, address,
+              email address, and telephone number.
+            </p>
+
+            <h2 className="text-xl font-bold mb-2">
+              How we use your information
+            </h2>
+
+            <p>
+              We use the information we collect in various ways, including to:
+            </p>
+
+            <ul className="mb-5">
+              <li>Provide, operate, and maintain our website</li>
+              <li>Improve, personalize, and expand our website</li>
+              <li>Understand and analyze how you use our website</li>
+              <li>
+                Develop new products, services, features, and functionality
+              </li>
+              <li>
+                Communicate with you, either directly or through one of our
+                partners, including for customer service, to provide you with
+                updates and other information relating to the website, and for
+                marketing and promotional purposes
+              </li>
+              <li>Send you emails</li>
+              <li>Find and prevent fraud</li>
+            </ul>
+
+            <h2 className="text-xl font-bold mb-2">Log Files</h2>
+
+            <p className="mb-5">
+              Mom&apos;s Corner follows a standard procedure of using log files.
+              These files log visitors when they visit websites. All hosting
+              companies do this and a part of hosting services analytics. The
+              information collected by log files include internet protocol (IP)
+              addresses, browser type, Internet Service Provider (ISP), date and
+              time stamp, referring/exit pages, and possibly the number of
+              clicks. These are not linked to any information that is personally
+              identifiable. The purpose of the information is for analyzing
+              trends, administering the site, tracking users movement on the
+              website, and gathering demographic information.
+            </p>
+
+            <h2 className="text-xl font-bold mb-2">
+              Google DoubleClick DART Cookie
+            </h2>
+
+            <p className="mb-5">
+              Google is one of a third-party vendor on our site. It also uses
+              cookies, known as DART cookies, to serve ads to our site visitors
+              based upon their visit to www.website.com and other sites on the
+              internet. However, visitors may choose to decline the use of DART
+              cookies by visiting the Google ad and content network Privacy
+              Policy at the following URL –{" "}
+              <a href="https://policies.google.com/technologies/ads">
+                https://policies.google.com/technologies/ads
+              </a>
+            </p>
+
+            <h2 className="text-xl font-bold mb-2">Our Advertising Partners</h2>
+
+            <p>
+              Some of advertisers on our site may use cookies and web beacons.
+              Our advertising partners are listed below. Each of our advertising
+              partners has their own Privacy Policy for their policies on user
+              data. For easier access, we hyperlinked to their Privacy Policies
+              below.
+            </p>
+
+            <ul className="mb-5">
+              <li>
+                <p>Google</p>
+                <p>
+                  <a href="https://policies.google.com/technologies/ads">
+                    https://policies.google.com/technologies/ads
+                  </a>
+                </p>
+              </li>
+            </ul>
+
+            <h2 className="text-xl font-bold mb-2">
+              Advertising Partners Privacy Policies
+            </h2>
+
+            <p className="mb-2">
+              You may consult this list to find the Privacy Policy for each of
+              the advertising partners of Mom&apos;s Corner. Third-party ad
+              servers or ad networks uses technologies like cookies, JavaScript,
+              or Web Beacons that are used in their respective advertisements
+              and links that appear on Mom&apos;s Corner, which are sent
+              directly to users browser. They automatically receive your IP
+              address when this occurs. These technologies are used to measure
+              the effectiveness of their advertising campaigns and/or to
+              personalize the advertising content that you see on websites that
+              you visit.
+            </p>
+
+            <p className="mb-5">
+              Note that Mom&apos;s Corner has no access to or control over these
+              cookies that are used by third-party advertisers.
+            </p>
+
+            <h2 className="text-xl font-bold mb-2">
+              Third Party Privacy Policies
+            </h2>
+
+            <p className="mb-2">
+              Mom&apos;s Corner Privacy Policy does not apply to other
+              advertisers or websites. Thus, we are advising you to consult the
+              respective Privacy Policies of these third-party ad servers for
+              more detailed information. It may include their practices and
+              instructions about how to opt-out of certain options.{" "}
+            </p>
+
+            <p className="mb-5">
+              You can choose to disable cookies through your individual browser
+              options. To know more detailed information about cookie management
+              with specific web browsers, it can be found at the browsers
+              respective websites.
+            </p>
+
+            <h2 className="text-xl font-bold mb-2">
+              GDPR Data Protection Rights
+            </h2>
+
+            <p className="mb-2">
+              We would like to make sure you are fully aware of all of your data
+              protection rights. Every user is entitled to the following:
+            </p>
+            <li className="mb-2">
+              The right to access – You have the right to request copies of your
+              personal data. We may charge you a small fee for this service.
+            </li>
+            <li className="mb-2">
+              The right to rectification – You have the right to request that we
+              correct any information you believe is inaccurate. You also have
+              the right to request that we complete the information you believe
+              is incomplete.
+            </li>
+            <li className="mb-2">
+              The right to erasure – You have the right to request that we erase
+              your personal data, under certain conditions.
+            </li>
+            <li className="mb-2">
+              The right to restrict processing – You have the right to request
+              that we restrict the processing of your personal data, under
+              certain conditions.
+            </li>
+            <li className="mb-2">
+              The right to object to processing – You have the right to object
+              to our processing of your personal data, under certain conditions.
+            </li>
+            <li className="mb-2">
+              The right to data portability – You have the right to request that
+              we transfer the data that we have collected to another
+              organization, or directly to you, under certain conditions.
+            </li>
+            <li className="mb-5">
+              If you make a request, we have one month to respond to you. If you
+              would like to exercise any of these rights, please contact us.
+            </li>
+
+            <h2 className="text-xl font-bold mb-2">
+              Children&apos;s Information
+            </h2>
+
+            <p className="mb-2">
+              Another part of our priority is adding protection for children
+              while using the internet. We encourage parents and guardians to
+              observe, participate in, and/or monitor and guide their online
+              activity.
+            </p>
+
+            <p>
+              Mom&apos;s Corner does not knowingly collect any Personal
+              Identifiable Information from children under the age of 13. If you
+              think that your child provided this kind of information on our
+              website, we strongly encourage you to contact us immediately and
+              we will do our best efforts to promptly remove such information
+              from our records.
+            </p>
+          </div>
+        </label>
+      </label>
+      {/* ---privay-policy---- */}
+
+      <span className="font-bold hover:underline cursor-pointer">
+        <label htmlFor="my-modal-2">Privacy Policy</label>
+      </span>
+    </>
+  );
+};
+
+export default Privacy;
